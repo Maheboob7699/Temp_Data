50cf7b9eeab3e27a21ccec678916c3ea8bdc68e7
Maheboob7699 <murdimaheboob.com>
2025-03-12 09:34:13 +0530

    changes in div section

--- src/components/PreviousRank.jsx
+++ src/components/PreviousRank.jsx
@@ -2,8 +2,20 @@ import '../assets/styles/PreviousRank.css';
 function PreviousRank({user}){
     return(
         <>
-         <div className='lower-rank'>
-             <h2 style={user?{}:{margin:'auto'}}>{user ? user.name:"user not found"}</h2>
+         <div className='lower-rank' style={{display:'flex',justifyContent:'space-evenly'}}>
+             {/* <h2 style={user?{justifyContent:'space-evenly'}:{margin:'auto'}}>{user ? user.name:"user not found"}</h2> */}
+
+             {user ? (
+                 <>
+                  <h2>{user.name}</h2>
+                  <h2>{user.email}</h2>
+                  <h2>{user.score}</h2>
+                 </>
+                 )
+                 : (
+                    <h2>user not found</h2>
+                 )
+             }
          </div>
         </>
     )
